cc650565b259814698bdd350fc6aba5036ba7e67
ElSono Sapiens <dihf.47@gmail.com>
2022-07-04 12:09:24 -0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+function Navbar() {
+  return (
+    <header id="navBar">
+      <h2>KohaiCan</h2>
+      <li id="navBarList">
+        <ul>Collares</ul>
+        <ul>Correas</ul>
+        <ul>Juguetes</ul>
+        <ul>Alimento</ul>
+      </li>
+    </header>
+  );
+}
+export default Navbar;
