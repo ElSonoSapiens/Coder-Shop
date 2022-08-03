58b95fca4884bf2a3d6b1c4d0da2da0fafb8be6e
ElSonoSapiens <dihf.47@gmail.com>
2022-08-03 07:52:25 -0300

    desafio-9-cartView-cartwidgetActualizadoConLink

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,16 +3,20 @@ import cart from "../assets/img/cart.svg";
 import "../styles/CartWidget.css";
 import { CartContext } from "./CartContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
   const cartContext = useContext(CartContext);
+
   return (
-    <div className="widget">
-      <img src={cart} alt="Cart" className="navbarCart" />
-      {cartContext.cartList.length > 0 && (
-        <p className="sumQty">{cartContext.sumQty()}</p>
-      )}
-    </div>
+    <Link to="/cart">
+      <div className="widget">
+        <img src={cart} alt="Cart" className="navbarCart" />
+        {cartContext.cartList.length > 0 && (
+          <p className="sumQty">{cartContext.sumQty()}</p>
+        )}
+      </div>
+    </Link>
   );
 };
 
